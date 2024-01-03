421a047d45c23db1f33d513ae60b4c65e566b7bb
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-03 15:20:39 +0000

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,18 +5,18 @@
  * @parent: is a pointer to the parent node of the node to create.
  * @value: is the value to put in the new node.
  * Return:Your function must return a pointer to the new node or NULL if no.
-*/
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-  binary_tree_t *link = malloc(sizeof(binary_tree_t));
+	binary_tree_t *link = malloc(sizeof(binary_tree_t));
 
-  if (link == NULL)
-  {
-    return (NULL);
-  }
-  link->n = value;
-  link->parent = parent;
-  link->left = NULL;
-  link->right = NULL;
-  return (link);
+	if (link == NULL)
+	{
+		return (NULL);
+	}
+	link->n = value;
+	link->parent = parent;
+	link->left = NULL;
+	link->right = NULL;
+	return (link);
 }
