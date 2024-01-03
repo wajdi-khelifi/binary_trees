0197056715f38b2f81745ea7b0d386a078ae8aee
seifbenothman <seifbenothman95@gmail.com>
2024-01-03 15:24:41 +0000

    0

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,23 +8,23 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_links;
 
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
 
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
+	new_links = binary_tree_node(parent, value);
+	if (new_links == NULL)
 	{
 		return (NULL);
 	}
 	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
+		new_links->left = parent->left;
+		parent->left->parent = new_links;
 	}
-	parent->left = new_node;
-	return (new_node);
+	parent->left = new_links;
+	return (new_links);
 }
